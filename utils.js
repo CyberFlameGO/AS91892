7384ea66f19c4ae8eb97807a0a041e3720acfc51
CyberFlame <cyberflameu@gmail.com>
2022-05-19 10:11:51 +1200

    ococodldld

--- utils.js
+++ utils.js
@@ -1,8 +1,19 @@
 function changeImage() {
 
-    if (document.getElementById("swapImg").src == "https://www.paghat.com/empireofthechihuahua/chi-by-raptorwitness.jpg") {
+    if (document.getElementById("swapImg").src === "https://www.paghat.com/empireofthechihuahua/chi-by-raptorwitness.jpg") {
         document.getElementById("swapImg").src = "http://i.huffpost.com/gen/1619949/thumbs/o-121255937-570.jpg?1";
     } else {
         document.getElementById("swapImg").src = "https://www.paghat.com/empireofthechihuahua/chi-by-raptorwitness.jpg";
     }
+}
+
+
+//const clickElement = document.getElementById("click-counter")
+const clickCount = document.getElementById("clicks")
+
+let clicks = 0;
+
+function incrementClickCount() {
+    clicks++
+    clickCount.innerHTML = clicks;
 }
