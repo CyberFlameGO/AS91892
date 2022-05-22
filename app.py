a7be58687e922a2d31bac1dafbdd0d96ff6be942
CyberFlame <cyberflameu@gmail.com>
2022-05-23 11:15:44 +1200

    cool

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/meme')
 def hello_world():
     """
     deez ;ntus
     :return:
     """
-    return str(request.args)
+    return ' '.join([*request.args.to_dict()])
 
+@app.route('/')
+def hello_uwu():
+    return render_template("index.html")
+
+@app.route('/contact')
+def hello_owo():
+    return render_template("contact.html")
 
 if __name__ == '__main__':
     app.run()
