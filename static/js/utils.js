c81f65ebe69802f58b56cf3015426eac93112d0f
CyberFlame <cyberflameu@gmail.com>
2022-05-26 10:08:32 +1200

    https

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,7 +1,7 @@
 function changeImage() {
 
     if (document.getElementById("swapImg").src === "https://www.paghat.com/empireofthechihuahua/chi-by-raptorwitness.jpg") {
-        document.getElementById("swapImg").src = "http://i.huffpost.com/gen/1619949/thumbs/o-121255937-570.jpg?1";
+        document.getElementById("swapImg").src = "https://i.huffpost.com/gen/1619949/thumbs/o-121255937-570.jpg?1";
     } else {
         document.getElementById("swapImg").src = "https://www.paghat.com/empireofthechihuahua/chi-by-raptorwitness.jpg";
     }
