4d0d03ccb0ac4f6473af66e6a920bdd7e63decd9
CyberFlame <cyberflameu@gmail.com>
2022-05-26 11:51:56 +1200

    fix lint?

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,19 +1,17 @@
-function changeImage() {
-
-    if (document.getElementById("swapImg").src === "https://www.paghat.com/empireofthechihuahua/chi-by-raptorwitness.jpg") {
-        document.getElementById("swapImg").src = "https://i.huffpost.com/gen/1619949/thumbs/o-121255937-570.jpg?1";
-    } else {
-        document.getElementById("swapImg").src = "https://www.paghat.com/empireofthechihuahua/chi-by-raptorwitness.jpg";
-    }
+function changeImage () { // eslint-disable-line no-unused-vars
+    
+  if (document.getElementById("swapImg").src === 'https://www.paghat.com/empireofthechihuahua/chi-by-raptorwitness.jpg') {
+      document.getElementById("swapImg").src = 'https://i.huffpost.com/gen/1619949/thumbs/o-121255937-570.jpg?1'
+  } else {
+      document.getElementById("swapImg").src = 'https://www.paghat.com/empireofthechihuahua/chi-by-raptorwitness.jpg'
+  }
 }
 
-
-//const clickElement = document.getElementById("click-counter")
-const clickCount = document.getElementById("clicks");
+const clickCount = document.getElementById('clicks');
 
 let clicks = 0;
 
-function incrementClickCount() {
-    clicks++;
-    clickCount.innerHTML = clicks;
+function incrementClickCount () { // eslint-disable-line no-unused-vars
+  clicks++;
+  clickCount.innerHTML = clicks;
 }
