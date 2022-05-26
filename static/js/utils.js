630d8ce1009b394f6c811fa26ece74d72da34e04
CyberFlame <cyberflameu@gmail.com>
2022-05-26 12:01:46 +1200

    Update utils.js

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,17 +1,16 @@
 function changeImage () { // eslint-disable-line no-unused-vars
-    
-  if (document.getElementById("swapImg").src === 'https://www.paghat.com/empireofthechihuahua/chi-by-raptorwitness.jpg') {
-      document.getElementById("swapImg").src = 'https://i.huffpost.com/gen/1619949/thumbs/o-121255937-570.jpg?1'
+  if (document.getElementById('swapImg').src === 'https://www.paghat.com/empireofthechihuahua/chi-by-raptorwitness.jpg') {
+    document.getElementById('swapImg').src = 'https://i.huffpost.com/gen/1619949/thumbs/o-121255937-570.jpg?1'
   } else {
-      document.getElementById("swapImg").src = 'https://www.paghat.com/empireofthechihuahua/chi-by-raptorwitness.jpg'
+    document.getElementById('swapImg').src = 'https://www.paghat.com/empireofthechihuahua/chi-by-raptorwitness.jpg'
   }
 }
 
-const clickCount = document.getElementById('clicks');
+const clickCount = document.getElementById('clicks')
 
-let clicks = 0;
+let clicks = 0
 
 function incrementClickCount () { // eslint-disable-line no-unused-vars
-  clicks++;
-  clickCount.innerHTML = clicks;
+  clicks++
+  clickCount.innerHTML = clicks
 }
